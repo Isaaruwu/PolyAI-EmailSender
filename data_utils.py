3c37b02d79e351ca76381f039ed5979759001ae2
Isaaruwu <ismailaarabb@gmail.com>
2023-05-26 19:15:20 -0400

    ✅  Added function to process the xlx

---
+++ data_utils.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def transform_data(csv_file: str, columns_to_remove: list[int]=None) -> pd.DataFrame:
+    df = pd.read_csv(csv_file)
+    df = df.drop(df.columns[columns_to_remove], axis=1) if columns_to_remove else df
+
+    return df.dropna()
