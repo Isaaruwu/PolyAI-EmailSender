403f3cbc5c91fb1c4d3c615a242974a2d66e7b76
Isaaruwu <ismailaarabb@gmail.com>
2023-05-26 19:17:24 -0400

    🎉 Added main function

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-"# PolyAI-EmailSender" 
+from data_utils import transform_data
+
+if __name__ == "__main__":
+    df = transform_data('./data/list_sponsorship.csv', [1, 2, 3, 4, 7, 8, 9, 10])
+    print(df)
